e399d24f5b5b379369203e43c048b22be077e695
mohamed medhat <73649080+CoDeBrEaKe@users.noreply.github.com>
2025-05-13 16:09:40 +0300

    Implemented Navbar , Hero section

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// Handling Menu 
+const burgerIcon = document.querySelector("header .burger-icon")
+const menu = document.querySelector('.menu')
+const close = document.querySelector('.close')
+burgerIcon.addEventListener('click' , ()=>{
+    document.body.style.overflow = " hidden";
+    setTimeout( ()=>{
+        menu.style.opacity = 100
+    }, 0)
+    menu.style.display = 'flex'
+})
+close.addEventListener('click' , ()=>{
+    document.body.style.overflow = "visible"
+    menu.style.opacity = 0
+    setTimeout(()=>{
+        menu.style.display = 'none'
+    }, 0)
+
+})
+// Small check to handle the dissappearance of navbar while resizing
+window.addEventListener('resize' , (e)=>{
+    if(window.innerWidth > 1024){
+        menu.style.opacity = 100
+    }else{
+        
+        menu.style.opacity = 100
+    }
+})
